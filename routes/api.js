85626799fa11f302d574aae83c67fd30e0a9e6e0
JameskuttySebastian <jameskutty.sebastian@gmail.com>
2020-05-04 10:58:54 +0930

    added detail comments

--- routes/api.js
+++ routes/api.js
@@ -23,7 +23,6 @@ router.post("/api/transaction/bulk", ({body}, res) => {
     });
 });
 
-// get a single transaction
 router.get("/api/transaction", (req, res) => {
   Transaction.find({}).sort({date: -1})
     .then(dbTransaction => {
